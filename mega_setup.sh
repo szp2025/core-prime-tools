217ffa226b6ab43693ce358e612154608c118d78
szp2025 <szymanskipawel1441@gmail.com>
2026-04-14 08:03:00 +0200

    Update mega_setup.sh

--- mega_setup.sh
+++ mega_setup.sh
@@ -8,7 +8,7 @@ SHARE_DIR="$BASE_DIR/share/nmap"
 # Используем исправленный системный WGET
 WGET="wget --no-check-certificate -q"
 
-echo "[*] Запуск неубиваемого установщика [88/90/95]..."
+echo "[*] Запуск неубиваемого установщика [88/90/95/PKG]..."
 mkdir -p "$BIN_DIR" "$SHARE_DIR"
 
 # --- УМНАЯ ФУНКЦИЯ СКАНЕРА ССЫЛОК ---
@@ -38,32 +38,38 @@ download_smart() {
     return 1
 }
 
+# --- 0. ОЖИВЛЯЕМ PKG (РЕПОЗИТОРИИ) ---
+echo "[*] Настройка репозиториев для Android 5.1..."
+if [ -d "$PREFIX/etc/apt" ]; then
+    echo "deb https://termux.pwn.net/termux-main-21 stable main" > "$PREFIX/etc/apt/sources.list"
+    # Добавляем альтернативное зеркало Grimler
+    echo "deb https://main.termux-mirror.ml stable main" >> "$PREFIX/etc/apt/sources.list"
+fi
+
 # --- 1. ЯДРО (БИНАРНИКИ ARM) ---
 cd "$BIN_DIR"
 
-# NMAP (Используем armel версии как самые стабильные)
+# NMAP
 download_smart "nmap" "Nmap" \
     "https://github.com/therealsaumil/static-arm-bins/raw/master/nmap-7.01-armel-static" \
-    "https://github.com/vlad-s/static-binaries/raw/master/nmap-arm" \
-    "https://github.com/optiv/static-binaries/raw/master/nmap-arm"
+    "https://github.com/vlad-s/static-binaries/raw/master/nmap-arm"
 
-# SOCAT (Версия 1.7.3.2)
+# SOCAT (Твой рабочий 1.7.3.2)
 download_smart "socat" "Socat" \
     "https://github.com/therealsaumil/static-arm-bins/raw/master/socat-armel-static" \
-    "https://github.com/m-p-h-c/static-binaries/raw/master/socat-armv7l" \
-    "https://github.com/3ndG4me/socat-static-binary/raw/master/socat-arm-static"
+    "https://github.com/m-p-h-c/static-binaries/raw/master/socat-armv7l"
 
 # TCPDUMP
 download_smart "tcpdump" "Tcpdump" \
     "https://github.com/therealsaumil/static-arm-bins/raw/master/tcpdump-4.7.4-armel-static" \
     "https://github.com/vlad-s/static-binaries/raw/master/tcpdump-arm"
 
-# AIRCRACK-NG (Добавил живые зеркала и HTTP-версии)
+# AIRCRACK-NG (Смешанные ссылки для пробива)
 download_smart "aircrack-ng" "Aircrack" \
+    "http://distro.ibiblio.org/fatdog/arm/packages/700/aircrack-ng-1.2-rc1-arm-1.txz" \
     "https://github.com/theMiddleBlue/static-binaries/raw/master/aircrack-ng-arm" \
     "https://github.com/JofreSastre/static-binaries/raw/master/aircrack-ng-arm" \
-    "https://github.com/therealsaumil/static-arm-bins/raw/master/aircrack-ng-1.2-rc4-armel-static" \
-    "http://distro.ibiblio.org/fatdog/arm/packages/700/aircrack-ng-1.2-rc1-arm-1.txz"
+    "http://andpwn.com/binaries/aircrack-ng-arm"
 
 # --- 2. БАЗЫ ---
 cd "$SHARE_DIR"
@@ -79,6 +85,5 @@ echo "alias статус='echo \"Проверка...\"; [ -f $BIN_DIR/nmap ] && 
 chmod 755 "$BASE_DIR/env.sh"
 
 echo " "
-echo "[УСПЕХ] Арсенал проверен."
-echo "Введи команду для активации:"
-echo "source $BASE_DIR/env.sh"
+echo "[УСПЕХ] Все ссылки добавлены. PKG настроен."
+echo "Введи: source $BASE_DIR/env.sh"
