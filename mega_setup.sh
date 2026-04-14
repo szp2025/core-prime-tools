95443c79832638e35d71a1e802abc439b8268473
szp2025 <szymanskipawel1441@gmail.com>
2026-04-14 02:03:25 +0200

    Update mega_setup.sh

--- mega_setup.sh
+++ mega_setup.sh
@@ -5,9 +5,9 @@ BASE_DIR="/data/local/tmp"
 BIN_DIR="$BASE_DIR/bin"
 SHARE_DIR="$BASE_DIR/share/nmap"
 
-# Находим BusyBox
-[ -f "$BIN_DIR/busybox" ] && BB="$BIN_DIR/busybox" || BB="busybox"
-WGET="$BB ./wget --no-check-certificate -q"
+# ИСПОЛЬЗУЕМ ИСПРАВЛЕННЫЙ СИСТЕМНЫЙ WGET (который мы подменили)
+# Больше не полагаемся на BusyBox для загрузки
+WGET="wget --no-check-certificate -q"
 
 echo "[*] Запуск неубиваемого установщика [88/90/95]..."
 mkdir -p $BIN_DIR $SHARE_DIR
@@ -25,6 +25,8 @@ download_smart() {
 
     for URL in "$@"; do
         echo "[*] Пробую зеркало для $NAME..."
+        # Пытаемся скачать. Если wget ругается на https, 
+        # он просто перейдет к следующей ссылке
         if $WGET "$URL" -O "$FILE_PATH"; then
             if [ -s "$FILE_PATH" ]; then
                 echo "[+] $NAME успешно загружен!"
@@ -32,7 +34,7 @@ download_smart() {
                 return 0
             fi
         fi
-        echo "[!] Ссылка не сработала, ищу дальше..."
+        echo "[!] Ссылка не сработала или протокол не поддерживается..."
     done
 
     echo "[!!!] Ошибка: Ни одна ссылка для $NAME не доступна."
@@ -42,32 +44,32 @@ download_smart() {
 # --- 1. ЯДРО (БИНАРНИКИ ARM) ---
 cd $BIN_DIR
 
-# NMAP (Зеркала: vlad-s, andrew-d, termux-bin)
+# NMAP
 download_smart "nmap" "Nmap" \
-    "https://github.com/vlad-s/static-binaries/raw/master/nmap-arm" \
-    "https://github.com/andrew-d/static-binaries/raw/master/binaries/linux/armel/nmap" \
-    "https://raw.githubusercontent.com/optiv/static-binaries/master/nmap-arm"
+    "https://github.com/therealsaumil/static-arm-bins/raw/master/nmap-7.01-armel-static" \
+    "https://github.com/vlad-s/static-binaries/raw/master/nmap-arm"
 
-# SOCAT (Сердце Модуля [95])
+# SOCAT (Рабочая ссылка от therealsaumil)
 download_smart "socat" "Socat" \
     "https://github.com/therealsaumil/static-arm-bins/raw/master/socat-armel-static" \
-    "https://github.com/andrew-d/static-binaries/raw/master/binaries/linux/armel/socat"
+    "https://github.com/m-p-h-c/static-binaries/raw/master/socat-armv7l"
 
-# TCPDUMP (Модуль [88])
+# TCPDUMP
 download_smart "tcpdump" "Tcpdump" \
-    "https://github.com/vlad-s/static-binaries/raw/master/tcpdump-arm" \
-    "https://github.com/andrew-d/static-binaries/raw/master/binaries/linux/armel/tcpdump"
+    "https://github.com/therealsaumil/static-arm-bins/raw/master/tcpdump-4.7.4-armel-static" \
+    "https://github.com/vlad-s/static-binaries/raw/master/tcpdump-arm"
 
-# AIRCRACK-NG (Wifislax/Kali)
+# AIRCRACK-NG (Зеркало, которое должно ожить)
 download_smart "aircrack-ng" "Aircrack" \
-    "https://github.com/vlad-s/static-binaries/raw/master/aircrack-ng-arm" \
-    "https://raw.githubusercontent.com/theMiddleBlue/static-binaries/master/aircrack-ng-arm"
+    "https://github.com/JofreSastre/static-binaries/raw/master/aircrack-ng-arm" \
+    "https://github.com/theMiddleBlue/static-binaries/raw/master/aircrack-ng-arm"
 
-# --- 2. БАЗЫ (ОФИЦИАЛЬНЫЕ + ЗЕРКАЛА) ---
+# --- 2. БАЗЫ ---
 cd $SHARE_DIR
+# Используем HTTP, если HTTPS капризничает на базах
 download_smart "nmap-services" "Nmap Services" \
     "https://raw.githubusercontent.com/nmap/nmap/master/nmap-services" \
-    "https://svn.nmap.org/nmap/nmap-services"
+    "http://svn.nmap.org/nmap/nmap-services"
 
 # --- 3. ФИНАЛИЗАЦИЯ ---
 cat <<EOF > $BASE_DIR/env.sh
@@ -78,4 +80,3 @@ EOF
 chmod 755 $BASE_DIR/env.sh
 
 echo "[УСПЕХ] Арсенал проверен. Введи: source $BASE_DIR/env.sh"
-
