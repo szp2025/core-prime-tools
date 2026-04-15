a82cfc998c9c49999984c1ad45e3b96ab5f77e95
szp2025 <szymanskipawel1441@gmail.com>
2026-04-16 00:43:24 +0200

    Update mega_setup.sh

--- mega_setup.sh
+++ mega_setup.sh
@@ -28,36 +28,31 @@ K_PATH="\$HOME_PATH/kali-system/kali-armhf"
 BB_ORIGIN="\$HOME_PATH/busybox-static"
 BB_DEV="/dev/busybox-kali"
 
-# 1. Подготовка окружения
-echo "[*] Настройка системы..."
+# 1. Подготовка
 su -c "cp \$BB_ORIGIN \$BB_DEV && chmod 755 \$BB_DEV"
 su -c "mount -o remount,exec /data"
 
-# 2. Монтирование ресурсов
+# 2. Монтирование
 su -c "
     \$BB_DEV mount -o bind /dev \$K_PATH/dev
     \$BB_DEV mount -o bind /proc \$K_PATH/proc
     \$BB_DEV mount -o bind /sys \$K_PATH/sys
-    # Настройка DNS для работы интернета
-    echo 'nameserver 8.8.8.8' > \$K_PATH/etc/resolv.conf
 "
 
-echo "[+] ВХОД В KALI LINUX..."
+echo "[+] ВХОД В KALI (Интерактивный режим)..."
 
-# 3. Запуск с автоматической настройкой окружения
-# Мы объединяем все экспортные команды в одну строку инициализации
+# 3. Запуск с исправленными путями и флагом -i для приема команд
 su -c "\$BB_DEV chroot \$K_PATH /bin/sh -c '
     export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin;
     export TERM=xterm-256color;
     export HOME=/root;
+    export LC_ALL=C;
     cd /root;
-    /bin/sh -i
+    exec /bin/sh -i
 '"
 
-# 4. Очистка ресурсов после выхода (exit)
-echo "[*] Завершение работы и размонтирование..."
+# 4. Размонтирование
 su -c "umount -l \$K_PATH/dev; umount -l \$K_PATH/proc; umount -l \$K_PATH/sys"
 EOF
 
-# Даем права на запуск самого скрипта
 chmod +x start_kali.sh
