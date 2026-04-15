041b956867158b9745807f50eb59c082f17c1039
szp2025 <szymanskipawel1441@gmail.com>
2026-04-15 15:07:38 +0200

    Update mega_setup.sh

--- mega_setup.sh
+++ mega_setup.sh
@@ -1,31 +1,25 @@
 #!/system/bin/sh
 
-# Полные пути к файлам
 KALI_PATH="/data/data/com.termux/files/home/kali-system/kali-armhf"
-T_BIN="/data/data/com.termux/files/usr/bin"
-T_LIB="/data/data/com.termux/files/usr/lib"
-CHROOT="$T_BIN/chroot"
+BB_STATIC="/data/data/com.termux/files/home/busybox-static"
 
-echo "[*] Попытка прорыва через LD_PRELOAD..."
+echo "[*] Контекст: Режим выживания (Static BusyBox)..."
 
-# 1. Срочное монтирование
-mount -o bind /dev "$KALI_PATH/dev" 2>/dev/null
-mount -o bind /proc "$KALI_PATH/proc" 2>/dev/null
-mount -o bind /sys "$KALI_PATH/sys" 2>/dev/null
+# 1. Скачиваем статический BusyBox (если его еще нет)
+if [ ! -f "$BB_STATIC" ]; then
+    echo "[*] Загрузка статического инструментария..."
+    /data/data/com.termux/files/home/wget --no-check-certificate https://github.com/busybox-static/busybox-static/raw/master/busybox-armv7l -O "$BB_STATIC"
+    chmod 777 "$BB_STATIC"
+fi
 
-# 2. Исправление прав (на случай если su их заблокировал)
-chmod 755 "$CHROOT"
-chmod 755 "$T_LIB/libandroid-support.so"
+# 2. Монтирование через статический BB
+echo "[*] Подготовка файловой системы..."
+$BB_STATIC mount -o bind /dev "$KALI_PATH/dev" 2>/dev/null
+$BB_STATIC mount -o bind /proc "$KALI_PATH/proc" 2>/dev/null
+$BB_STATIC mount -o bind /sys "$KALI_PATH/sys" 2>/dev/null
 
-# 3. ЗАПУСК С ПРИНУДИТЕЛЬНОЙ ПОДГРУЗКОЙ БИБЛИОТЕК
-echo "[!] ВХОД В KALI..."
+# 3. ВХОД (Без внешних зависимостей)
+echo "[!] ГАМБИТ СРАБОТАЛ. ВХОДИМ..."
 
-# Это одна длинная команда, которая говорит руту: "Сначала возьми эту библиотеку, а потом запускай"
-LD_PRELOAD="$T_LIB/libandroid-support.so" \
-LD_LIBRARY_PATH="$T_LIB" \
-PATH="$T_BIN:/system/bin:/system/xbin" \
-"$CHROOT" "$KALI_PATH" /usr/bin/env -i \
-    HOME=/root \
-    TERM=xterm-256color \
-    PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin \
-    /bin/bash --login
+# Используем chroot именно из статического busybox
+$BB_STATIC chroot "$KALI_PATH" /bin/bash --login
