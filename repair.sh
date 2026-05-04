26065b098d40a6417911d76135cc1b4ce31b34f0
szp2025 <szymanskipawel1441@gmail.com>
2026-05-04 23:53:00 +0200

    Update repair.sh

--- repair.sh
+++ repair.sh
@@ -16,5 +16,3 @@ curl -L -o /root/install_all.sh https://raw.githubusercontent.com/szp2025/core-p
 chmod +x /root/install_all.sh
 
 echo -e "\e[1;32m[OK] Ремонт окончен. Теперь запускай: ./install_all.sh\e[0m"
-EOF
-chmod +x /root/repair.sh
