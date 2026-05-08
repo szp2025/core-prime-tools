0b2b8b75153e6b4ad36b343be95c24613ee009ea
szp2025 <szymanskipawel1441@gmail.com>
2026-05-08 10:11:13 +0200

    Update install_all.sh

--- install_all.sh
+++ install_all.sh
@@ -1122,7 +1122,7 @@ update_module "/root/share_server.py" "1.2" generate_share_server_code "File-Sha
 update_module "/root/upload_server.py"  "1.2" generate_upload_server_code  "Inbound-Drop-Box"
 
 # --- ВЫЗОВ В ИНСТАЛЛЕРЕ ---
-update_module "/root/launcher.sh" "32.0" generate_launcher_code "Prime-Launcher"
+update_module "/root/launcher.sh" "32.5" generate_launcher_code "Prime-Launcher"
 chmod +x /root/launcher.sh
 ln -sf /root/launcher.sh /usr/local/bin/launcher
 repair_and_clean
